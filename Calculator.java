87b1eb928fc67e84b3aa290643c11154c4425b71
C1\DS <uuwa1034@naver.com>
2018-10-05 13:53:32 +0900

    sub() 등 추가

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,26 @@
 
 
 public class Calculator{
-    
+
     public static int sum(int a,int b){
 
             return a+b;
+    
+    }
+
+    public static int sub(int a,int b){
+
+            return a-b;
+    
+    }
+    public static int mul(int a,int b){
+
+            return a*b;
+    
+    }
+    public static int div(int a,int b){
+
+            return a/b;
     
     }
 
